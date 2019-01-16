b94d946960514425a23d21a8701681641ad885cb
Tomasz Kępa <misterecco@gmail.com>
2019-01-16 18:17:40 +0000

    Working VGA module
    For now it can only display three rectangles

--- gpu/top.v
+++ gpu/top.v
@@ -1,49 +1,51 @@
+`default_nettype none
+
 module top(
-	input wire PS2C,
-	input wire PS2D,
-	output wire [7:0] Led,
-	output wire HSYNC,
-	output wire VSYNC,
-	output wire [2:0] OutRed,
-	output wire [2:0] OutGreen,
-	output wire [2:1] OutBlue,
-	input wire [7:0] sw,
-	input wire [3:0] btn,
-	input wire uclk
+    output wire HSYNC,
+    output wire VSYNC,
+    output wire [2:0] OutRed,
+    output wire [2:0] OutGreen,
+    output wire [2:1] OutBlue,
+    output wire [6:0] seg,
+    output wire [3:0] an,
+    output wire [7:0] led,
+    input wire [7:0] sw,
+    input wire [3:0] btn,
+    input wire mclk
 );
 
 wire vclk;
 
-wire [7:0] kd;
-wire kv;
-
-wire we;
-wire [6:0] wx;
-wire [4:0] wy;
-wire [8:0] wd;
+assign led = sw;
 
 vga vga_inst (
-	.HS(HSYNC),
-	.VS(VSYNC),
-	.R(OutRed),
-	.G(OutGreen),
-	.B(OutBlue),
-	.we(we),
-	.wx(wx),
-	.wy(wy),
-	.wd(wd),
-	.clk(vclk)
+    .HS(HSYNC),
+    .VS(VSYNC),
+    .R(OutRed),
+    .G(OutGreen),
+    .B(OutBlue),
+    .sw(sw),
+    .clk(vclk),
+    .rst(0)
 );
 
 DCM_SP #(
-	.CLKFX_DIVIDE(26),
-	.CLKFX_MULTIPLY(23),
-	.CLKIN_PERIOD(31.25),
-	.CLK_FEEDBACK("NONE"),
-	.STARTUP_WAIT("TRUE")
+    .CLKFX_DIVIDE(10),
+    .CLKFX_MULTIPLY(2),
+    .CLKIN_PERIOD(10),
+    .CLK_FEEDBACK("NONE"),
+    .STARTUP_WAIT("TRUE")
 ) dcm_vclk (
-	.CLKFX(vclk),
-	.CLKIN(uclk)
+    .CLKFX(vclk),
+    .CLKIN(mclk)
+);
+
+display display_inst (
+    .number(16'h1234),
+    .seg(seg),
+    .an(an),
+    .empty(0),
+    .clk(mclk)
 );
 
 endmodule
--- gpu/vga.v
+++ gpu/vga.v
@@ -1,112 +1,66 @@
+`default_nettype none
+
 module vga(
-	output reg HS,
-	output reg VS,
+    output wire HS,
+    output wire VS,
 	output reg [2:0] R,
 	output reg [2:0] G,
 	output reg [2:1] B,
-	input wire we,
-	input wire [6:0] wx,
-	input wire [4:0] wy,
-	input wire [8:0] wd,
-	input wire clk
+    input wire [7:0] sw,
+    input wire clk,
+    input wire rst
 );
 
-parameter H_TOTAL = 100;
-parameter H_VISIBLE = 80;
-parameter H_SS = 82;
-parameter H_SE = 94;
+// VGA timings https://timetoexplore.net/blog/video-timings-vga-720p-1080p
+localparam HS_START = 16;              // horizontal sync start
+localparam HS_END = 16 + 96;         // horizontal sync end
+localparam HA_START = 16 + 96 + 48;    // horizontal active pixel start
+localparam LINE   = 800;             // complete line (pixels)
+localparam VA_END = 480;             // vertical active pixel end
+localparam VS_START = 480 + 11;        // vertical sync start
+localparam VS_END = 480 + 11 + 2;    // vertical sync end
+localparam SCREEN = 524;             // complete screen (lines)
 
-parameter V_TOTAL = 449;
-parameter V_VISIBLE = 400;
-parameter V_SS = 412;
-parameter V_SE = 414;
+reg [9:0] h_count;  // line position
+reg [9:0] v_count;  // screen position
+wire visible;
+wire [9:0] o_x;
+wire [9:0] o_y;
 
-reg [3:0] hpix;
-reg [6:0] hpos;
-reg [8:0] vpos;
+// generate sync signals (active low for 640x480)
+assign HS = ~((h_count >= HS_START) & (h_count < HS_END));
+assign VS = ~((v_count >= VS_START) & (v_count < VS_END));
 
-wire de;
+assign visible = h_count >= HA_START && v_count <= VA_END; 
 
-reg [8:0] font [2047:0];
-reg [8:0] framebuffer [2047:0];
+// keep x and y bound within the active pixels
+assign o_x = (h_count < HA_START) ? 0 : (h_count - HA_START);
+assign o_y = (v_count >= VA_END) ? (VA_END - 1) : (v_count);
 
-integer i;
+always @ (posedge clk)
+begin
+    if (rst) begin
+        h_count <= 0;
+        v_count <= 0;
+    end
+    begin
+        if (!visible) 
+            {R,G,B} <= 0;
+        else begin
+            R <= o_x <= 200 ? sw[7:5] : 0;
+            G <= o_x > 200 && o_x <= 400 ? sw[4:2] : 0;
+            B <= o_x > 400 ? sw[1:0] : 0;
+        end
+        
+        if (h_count == LINE) begin // end of line
+            h_count <= 0;
+            v_count <= v_count + 1;
+        end else 
+            h_count <= h_count + 1;
 
-initial $readmemh("font.bin", font);
-//initial $readmemh("fb.bin", framebuffer);
-
-reg [8:0] chr;
-reg [8:0] fontline;
-
-reg [3:0] hpix_1;
-reg [3:0] hpix_2;
-reg [8:0] vpos_1;
-reg de_2, de_1;
-reg hs_2, hs_1;
-reg vs_2, vs_1;
-wire hs_0;
-wire vs_0;
-
-always @(posedge clk) begin
-	// 1 <= 0
-	chr <= framebuffer[11'd80 * vpos[8:4] + hpos];
-	vpos_1 <= vpos;
-	hpix_1 <= hpix;
-	de_1 <= de;
-	hs_1 <= hs_0;
-	vs_1 <= vs_0;
-	// 2 <= 1
-	fontline <= font[{chr[6:0], vpos_1[3:0]}];
-	hpix_2 <= hpix_1;
-	de_2 <= de_1;
-	hs_2 <= hs_1;
-	vs_2 <= vs_1;
-	// 3 <= 2
-	HS <= hs_2;
-	VS <= vs_2;
-	if (de_2) begin
-		R <= fontline[hpix_2] ? 7 : 0;
-		G <= fontline[hpix_2] ? 7 : 0;
-		B <= fontline[hpix_2] ? 3 : 0;
-	end else begin
-		R <= 0;
-		G <= 0;
-		B <= 0;
-	end
-	if (we) begin
-		framebuffer[wx + wy * 11'd80] <= wd;
-	end
+        if (v_count == SCREEN)  // end of screen
+            v_count <= 0;
+    end
 end
-
-initial begin
-	hpos = 0;
-	vpos = 0;
-end
-
-assign hs_0 = ~(hpos >= H_SS && hpos < H_SE);
-assign vs_0 = (vpos >= V_SS && vpos < V_SE);
-
-assign de = hpos < H_VISIBLE && vpos < V_VISIBLE;
-
-always @(posedge clk) begin
-	if (hpix == 8) begin
-		hpix <= 0;
-		if (hpos == H_TOTAL - 1) begin
-			hpos <= 0;
-			if (vpos == V_TOTAL - 1)
-				vpos <= 0;
-			else
-				vpos <= vpos + 1;
-		end else begin
-			hpos <= hpos + 1;
-		end
-	end else begin
-		hpix <= hpix + 1;
-	end
-end
-
-//assign R = de ? hpix : 0;
-//assign G = de ? hpos : 0;
-//assign B = de ? vpos : 0;
 
 endmodule
